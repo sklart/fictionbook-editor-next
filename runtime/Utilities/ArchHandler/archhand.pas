897151e10d7dc2f26bad6af147876697f1996256
sklart <sklart@yandex.ru>
2026-08-08 16:41:17 +0300

    Fix ArchHandler configuration and launching

--- runtime/Utilities/ArchHandler/archhand.pas
+++ runtime/Utilities/ArchHandler/archhand.pas
@@ -1,41 +1,101 @@
-uses windows,shellapi;
+program ArchHandler;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Winapi.Windows,
+  Winapi.ShellAPI,
+  System.SysUtils,
+  System.Win.Registry;
+
+const
+  SettingsRoot = 'Software\\FictionBook Editor\\ArchHandler\\';
+
+function ReadSetting(const ArchiveType, Name: string): string;
+var
+  Registry: TRegistry;
+begin
+  Result := '';
+  Registry := TRegistry.Create(KEY_READ);
+  try
+    Registry.RootKey := HKEY_CURRENT_USER;
+    if Registry.OpenKeyReadOnly(SettingsRoot + ArchiveType) and
+       Registry.ValueExists(Name) then
+      Result := Registry.ReadString(Name);
+  finally
+    Registry.Free;
+  end;
+end;
+
+function IsFB2Archive(const ArchivePath: string): Boolean;
+var
+  NameWithoutArchiveExtension: string;
+begin
+  NameWithoutArchiveExtension := ChangeFileExt(ExtractFileName(ArchivePath), '');
+  Result := SameText(ExtractFileExt(NameWithoutArchiveExtension), '.fb2');
+end;
+
+function ExpandParameters(const Template, ArchivePath: string): string;
+begin
+  Result := StringReplace(Template, '$1', QuotedStr(ArchivePath), [rfReplaceAll]);
+end;
+
+procedure FailLaunch(const ProgramPath, ArchivePath: string; ErrorCode: NativeInt);
+begin
+  MessageBoxW(0, PWideChar(Format(
+    'Не удалось открыть архив.' + sLineBreak + sLineBreak +
+    'Программа: %s' + sLineBreak +
+    'Архив: %s' + sLineBreak +
+    'Код ShellExecute: %d', [ProgramPath, ArchivePath, ErrorCode])),
+    'ArchHandler', MB_OK or MB_ICONERROR);
+  Halt(1);
+end;
+
+procedure LaunchArchive(const ArchiveType, ArchivePath: string);
+var
+  ProgramPath, Parameters: string;
+  LaunchResult: HINST;
+begin
+  if IsFB2Archive(ArchivePath) then
+  begin
+    ProgramPath := ReadSetting(ArchiveType, 'FB2Program');
+    Parameters := ReadSetting(ArchiveType, 'FB2Parameters');
+  end
+  else
+  begin
+    ProgramPath := ReadSetting(ArchiveType, 'ArchiveProgram');
+    Parameters := ReadSetting(ArchiveType, 'ArchiveParameters');
+  end;
+
+  if (ProgramPath = '') or not FileExists(ProgramPath) then
+    FailLaunch(ProgramPath, ArchivePath, ERROR_FILE_NOT_FOUND);
+
+  LaunchResult := ShellExecuteW(0, 'open', PWideChar(ProgramPath),
+    PWideChar(ExpandParameters(Parameters, ArchivePath)),
+    PWideChar(ExtractFilePath(ProgramPath)), SW_SHOWNORMAL);
+  if NativeInt(LaunchResult) <= 32 then
+    FailLaunch(ProgramPath, ArchivePath, NativeInt(LaunchResult));
+end;
 
 var
-  s: string;
-  params: string;
-  param2,param4:string;
-  c : integer;
-
+  ArchiveType, ArchivePath: string;
 begin
-  if (paramcount<5) then
+  if (ParamCount <> 3) or not SameText(ParamStr(1), '--type') then
   begin
-    MessageBox(0,'ArchHandler needs at least five parameters to be passed.','ArchHandler Error.',MB_OK);
-    halt;
+    MessageBoxW(0, 'Использование: ArchHandler.exe --type rar|zip "архив"',
+      'ArchHandler', MB_OK or MB_ICONERROR);
+    Halt(2);
   end;
-  s:=paramstr(1);
-  if (pos('.fb2.',s)>0) or (pos('.Fb2.',s)>0) or (pos('.fB2.',s)>0) or (pos('.FB2.',s)>0) then
+
+  ArchiveType := LowerCase(ParamStr(2));
+  ArchivePath := ParamStr(3);
+  if not ((ArchiveType = 'rar') or (ArchiveType = 'zip')) then
   begin
-    params:=paramstr(3);
-    c:=pos('$1',params);
-    while (c>0) do
-    begin
-      params := copy(params,1,c-1)+#34+paramstr(1)+#34+copy(params,c+2,length(params)-c-2);
-      c:=pos('$1',params);
-    end;
-    params:=params+#0;
-    param2:=paramstr(2)+#0;
-    ShellExecute(0,'open',@param2[1],@params[1],@param2[1],sw_shownormal);
-  end
-  else begin
-    params:=paramstr(5);
-    c:=pos('$1',params);
-    while (c>0) do
-    begin
-      params := copy(params,1,c-1)+#34+paramstr(1)+#34+copy(params,c+2,length(params)-c-2);
-      c:=pos('$1',params);
-    end;
-    params:=params+#0;
-    param4:=paramstr(4)+#0;
-    ShellExecute(0,'open',@param4[1],@params[1],@param4[1],sw_shownormal);
+    MessageBoxW(0, 'Поддерживаются только типы rar и zip.', 'ArchHandler',
+      MB_OK or MB_ICONERROR);
+    Halt(2);
   end;
+  if not FileExists(ArchivePath) then
+    FailLaunch('', ArchivePath, ERROR_FILE_NOT_FOUND);
+  LaunchArchive(ArchiveType, ArchivePath);
 end.
