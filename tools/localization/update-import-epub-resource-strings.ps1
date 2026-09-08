2d1d68036d2f2ae306fd0af323f11d53e249c09b
sklart <sklart@yandex.ru>
2026-09-08 17:55:24 +0300

    Read EPUB localization catalogs as UTF-8

--- tools/localization/update-import-epub-resource-strings.ps1
+++ tools/localization/update-import-epub-resource-strings.ps1
@@ -52,7 +52,7 @@ function ConvertTo-RcStringLiteral {
     return $value
 }
 
-$catalog = Get-Content -Raw -LiteralPath $CatalogPath | ConvertFrom-Json
+$catalog = Get-Content -Raw -LiteralPath $CatalogPath -Encoding UTF8 | ConvertFrom-Json
 $entries = @(
     $catalog.strings.PSObject.Properties |
         Where-Object {
