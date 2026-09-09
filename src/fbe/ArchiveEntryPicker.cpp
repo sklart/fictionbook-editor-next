5e1b75c6858cac5a1bca17918a7ed1b5da968dc5
sklart <sklart@yandex.ru>
2026-09-09 23:05:46 +0300

    feat: localize archive entry picker

--- src/fbe/ArchiveEntryPicker.cpp
+++ src/fbe/ArchiveEntryPicker.cpp
@@ -1,13 +1,17 @@
 #include "stdafx.h"
 #include "ArchiveEntryPicker.h"
+#include "RuntimeLocalization.h"
 
 LRESULT CArchiveEntryPicker::OnInitDialog(UINT, WPARAM, LPARAM, BOOL&)
 {
+	SetWindowText(FbeLoadRuntimeStringByKey(L"fbe.archive.picker.caption", L"Select a FictionBook document"));
+	::SetWindowText(GetDlgItem(IDOK), FbeLoadRuntimeStringByKey(L"fbe.archive.picker.open", L"Open"));
+	::SetWindowText(GetDlgItem(IDCANCEL), FbeLoadRuntimeStringByKey(L"fbe.archive.picker.cancel", L"Cancel"));
 	m_list.Attach(GetDlgItem(IDC_ARCHIVE_ENTRY_LIST));
 	m_list.SetExtendedListViewStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
-	m_list.InsertColumn(0, L"Filename", LVCFMT_LEFT, 125);
-	m_list.InsertColumn(1, L"Internal path", LVCFMT_LEFT, 220);
-	m_list.InsertColumn(2, L"Size", LVCFMT_RIGHT, 65);
+	m_list.InsertColumn(0, FbeLoadRuntimeStringByKey(L"fbe.archive.picker.filename", L"Filename"), LVCFMT_LEFT, 125);
+	m_list.InsertColumn(1, FbeLoadRuntimeStringByKey(L"fbe.archive.picker.path", L"Internal path"), LVCFMT_LEFT, 220);
+	m_list.InsertColumn(2, FbeLoadRuntimeStringByKey(L"fbe.archive.picker.size", L"Size"), LVCFMT_RIGHT, 65);
 	for (size_t index = 0; index < m_entries.size(); ++index)
 	{
 		const FbeArchive::Entry& entry = m_entries[index];
