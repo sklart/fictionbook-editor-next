5cc8bf1a6cedd27be74990745229cee5d86ac54e
sklart <sklart@yandex.ru>
2026-09-15 04:10:44 +0300

    fix: advance script toolbar resource control id

--- src/fbe/resource.h
+++ src/fbe/resource.h
@@ -787,7 +787,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        261
 #define _APS_NEXT_COMMAND_VALUE         60000
-#define _APS_NEXT_CONTROL_VALUE         1642
+#define _APS_NEXT_CONTROL_VALUE         1651
 #define _APS_NEXT_SYMED_VALUE           133
 #endif
 #endif
