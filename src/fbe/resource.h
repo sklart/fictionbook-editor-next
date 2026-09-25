ac108b6c8321f33e2943ac42ef10dbda7f8a9423
sklart <sklart@yandex.ru>
2026-09-25 17:11:35 +0300

    Fix next script panel control ID

--- src/fbe/resource.h
+++ src/fbe/resource.h
@@ -788,7 +788,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        261
 #define _APS_NEXT_COMMAND_VALUE         57602
-#define _APS_NEXT_CONTROL_VALUE         1651
+#define _APS_NEXT_CONTROL_VALUE         1652
 #define _APS_NEXT_SYMED_VALUE           133
 #endif
 #endif
